aa97f585388230d874627224ed3a990593f67ad3
martin0330 <martin.cruz0330@yahoo.com>
2022-06-27 23:07:37 -0500

    deleted socket path

--- config/connection.js
+++ config/connection.js
@@ -8,13 +8,7 @@ const sequelize = process.env.JAWSDB_URL
     : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
         host: 'localhost',
         dialect: 'mysql',
-        port: 3306,
-        dialectOptions: {
-            socketPath: 'your socket path',
-            supportBigNumbers: true,
-            bigNumberStrings: true
-        },
-
+        port: 3306
     });
 
 module.exports = sequelize;
