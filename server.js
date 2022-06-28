dd45ac854d492812b5491325a71f46495c58cad4
martin0330 <martin.cruz0330@yahoo.com>
2022-06-27 22:41:21 -0500

    updated server

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
+
+
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const helpers = require('./utils/helper');
-
-
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -33,8 +33,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-app.use(require('./controllers'));
+app.use(require('./controllers/'));
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
